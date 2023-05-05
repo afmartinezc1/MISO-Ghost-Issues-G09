013856d21663eeb4805b7f6d9b5300c82a160cf5
Andres Martinez <af.martinezc1@uniandes.udu.co>
2023-05-04 22:59:54 -0500

    Creacion del pageElements y design feature

---
+++ EntregaSemana5/Kraken/features/web/step_definitions/pageElements.js
@@ -0,0 +1,15 @@
+const pageElements = {
+  login: {
+    userInput: "#ember8",
+    passInput: "#ember10",
+    loginBtn: "#ember12",
+  },
+  design: {
+    linkSideMenu: "#ember41",
+    menuItemNameInput: "#settings-navigation > .gh-blognav-item .gh-blognav-line span:first-of-type input",
+    menuItemUrlInput: "#settings-navigation > .gh-blognav-item .gh-blognav-line span:last-of-type input",
+    saveBtn: ".gh-btn-blue"
+  },
+};
+
+global.pageElements = pageElements
--- EntregaSemana5/Kraken/features/web/step_definitions/step.js
+++ EntregaSemana5/Kraken/features/web/step_definitions/step.js
@@ -1,16 +1,37 @@
 const { Given, When, Then } = require("@cucumber/cucumber");
+require("./pageElements");
 
 When("I enter email {kraken-string}", async function (email) {
-  let element = await this.driver.$("#ember8");
+  let element = await this.driver.$(global.pageElements.login.userInput);
   return await element.setValue(email);
 });
 
 When("I enter password {kraken-string}", async function (password) {
-  let element = await this.driver.$("#ember10");
+  let element = await this.driver.$(global.pageElements.login.passInput);
   return await element.setValue(password);
 });
 
 When("I click SignIn", async function () {
-  let element = await this.driver.$("#ember12");
+  let element = await this.driver.$(global.pageElements.login.loginBtn);
+  return await element.click();
+});
+
+When("I click in Design", async function () {
+  let element = await this.driver.$(global.pageElements.design.linkSideMenu);
+  return await element.click();
+});
+
+When("I fill name of menu item {string}", async function (msg) {
+  let element = await this.driver.$(global.pageElements.design.menuItemNameInput);
+  return await element.setValue(msg);
+});
+
+When("I fill page url {string}", async function (url) {
+  let element = await this.driver.$(global.pageElements.design.menuItemUrlInput);
+  return await element.setValue(url);
+});
+
+When("I save changes", async function () {
+  let element = await this.driver.$(global.pageElements.design.saveBtn);
   return await element.click();
 });
