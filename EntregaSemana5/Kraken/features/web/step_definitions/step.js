b31e0bd8ac9c9b55743d548a0f43d767f8ee5b7f
snader21 <s.nader21@uniandes.edu.co>
2023-05-05 11:04:05 -0500

    Se adicionan 6 escenarios de prueba para Crear, editar, borrar posts y pages

--- EntregaSemana5/Kraken/features/web/step_definitions/step.js
+++ EntregaSemana5/Kraken/features/web/step_definitions/step.js
@@ -41,30 +41,165 @@ When("I save changes", async function () {
   return await element.click();
 });
 
+// POSTS //
+When("I go to the posts view", async function () {
+  let element = await this.driver.$(global.pageElements.post.postView);
+  return await element.click();
+});
+
 When("I click new post", async function () {
   let element = await this.driver.$(global.pageElements.post.newPost);
   return await element.click();
 });
 
-When("I enter title {string}", async function (title) {
+When("I enter post title {string}", async function (title) {
   let element = await this.driver.$(global.pageElements.post.postTitle);
   return await element.setValue(title);
 });
 
-When("I enter content {kraken-string}", async function (content) {
+When("I enter post content {kraken-string}", async function (content) {
   let element = await this.driver.$(global.pageElements.post.postContent);
   return await element.setValue(content);
 });
 
-When("I click post to go to previous page", async function () {
+When("I click posts to go to previous page", async function () {
   let element = await this.driver.$(
     global.pageElements.post.newPostPreviousPage
   );
   return await element.click();
 });
 
-Then("I should see the draft post with title {string}", async function (title) {
-  let element = await this.driver.$(global.pageElements.post.firstPostInList);
-  const actualTitle = await element.getText();
+Then(
+  "I should see the first draft post with title {string}",
+  async function (title) {
+    let element = await this.driver.$(global.pageElements.post.firstPostInList);
+    const actualTitle = await element.getText();
+    expect(actualTitle).to.equal(title);
+  }
+);
+
+Then("I should see that the post title is {string}", async function (title) {
+  let element = await this.driver.$(global.pageElements.post.postTitle);
+  const actualTitle = await element.getValue();
   expect(actualTitle).to.equal(title);
+});
+
+Then(
+  "I should see that the edited post content is {kraken-string}{kraken-string}",
+  async function (content1, content2) {
+    let element = await this.driver.$(global.pageElements.post.postContent);
+    const actualContent = await element.getText();
+    const content = `${content1}${content2}`;
+    expect(actualContent).to.equal(content);
+  }
+);
+Then(
+  "I should see that the post content is {kraken-string}",
+  async function (content) {
+    let element = await this.driver.$(global.pageElements.post.postContent);
+    const actualContent = await element.getText();
+    expect(actualContent).to.equal(content);
+  }
+);
+
+When("I click on the first draft post", async function () {
+  let element = await this.driver.$(global.pageElements.post.firstPostInList);
+  return await element.click();
+});
+
+When("I click on post settings", async function () {
+  let element = await this.driver.$(global.pageElements.post.postSettings);
+  return await element.click();
+});
+
+When("I click on delete post", async function () {
+  let element = await this.driver.$(global.pageElements.post.deletePostBtn);
+  return await element.click();
+});
+
+When("I click on confirm post delete", async function () {
+  let element = await this.driver.$(global.pageElements.post.confirmDeleteBtn);
+  return await element.click();
+});
+
+// PAGES //
+When("I go to the pages view", async function () {
+  let element = await this.driver.$(global.pageElements.page.pageView);
+  return await element.click();
+});
+
+When("I click new page", async function () {
+  let element = await this.driver.$(global.pageElements.page.newPage);
+  return await element.click();
+});
+
+When("I enter page title {string}", async function (title) {
+  let element = await this.driver.$(global.pageElements.page.pageTitle);
+  return await element.setValue(title);
+});
+
+When("I enter page content {kraken-string}", async function (content) {
+  let element = await this.driver.$(global.pageElements.page.pageContent);
+  return await element.setValue(content);
+});
+
+When("I click pages to go to previous page", async function () {
+  let element = await this.driver.$(
+    global.pageElements.page.newPagePreviousPage
+  );
+  return await element.click();
+});
+
+Then(
+  "I should see the first draft page with title {string}",
+  async function (title) {
+    let element = await this.driver.$(global.pageElements.page.firstPageInList);
+    const actualTitle = await element.getText();
+    expect(actualTitle).to.equal(title);
+  }
+);
+
+Then("I should see that the page title is {string}", async function (title) {
+  let element = await this.driver.$(global.pageElements.page.pageTitle);
+  const actualTitle = await element.getValue();
+  expect(actualTitle).to.equal(title);
+});
+
+Then(
+  "I should see that the edited page content is {kraken-string}{kraken-string}",
+  async function (content1, content2) {
+    let element = await this.driver.$(global.pageElements.page.pageContent);
+    const actualContent = await element.getText();
+    const content = `${content1}${content2}`;
+    expect(actualContent).to.equal(content);
+  }
+);
+
+Then(
+  "I should see that the page content is {kraken-string}",
+  async function (content) {
+    let element = await this.driver.$(global.pageElements.page.pageContent);
+    const actualContent = await element.getText();
+    expect(actualContent).to.equal(content);
+  }
+);
+
+When("I click on the first draft page", async function () {
+  let element = await this.driver.$(global.pageElements.page.firstPageInList);
+  return await element.click();
+});
+
+When("I click on page settings", async function () {
+  let element = await this.driver.$(global.pageElements.page.pageSettings);
+  return await element.click();
+});
+
+When("I click on delete page", async function () {
+  let element = await this.driver.$(global.pageElements.page.deletePageBtn);
+  return await element.click();
+});
+
+When("I click on confirm page delete", async function () {
+  let element = await this.driver.$(global.pageElements.page.confirmDeleteBtn);
+  return await element.click();
 });
