98dae505c7bb22751e18097f0ac9ccc587b49ca1
snader21 <s.nader21@uniandes.edu.co>
2023-05-06 19:38:49 -0500

    Se crean 7 escenarios de prueba en cypress correspondientes a los escenarios de SN

--- EntregaSemana5/Cypress/pageElements.js
+++ EntregaSemana5/Cypress/pageElements.js
@@ -3,7 +3,7 @@ const pageElements = {
     body: "body",
     iframe: "iframe",
     siteTitle: ".gh-nav-menu-details-blog",
-    siteDescription: ".site-description"
+    siteDescription: ".site-description",
   },
   login: {
     userInput: "#ember8",
@@ -11,11 +11,11 @@ const pageElements = {
     loginBtn: "#ember12",
   },
   post: {
-    postView: "[href='#/posts/']",
-    newPost: "[href='#/editor/post/']",
+    postView: ".gh-nav-list-new [href='#/posts/']",
+    newPost: "[href='#/editor/post/'].gh-secondary-action",
     postTitle: "[placeholder='Post Title']",
     postContent: "[data-placeholder='Begin writing your post...']",
-    newPostPreviousPage: "[href='#/posts/']",
+    newPostPreviousPage: ".gh-editor-header [href='#/posts/']",
     firstPostInList: ".gh-list > li:nth-child(2) .gh-content-entry-title",
     postSettings: "button.post-settings",
     deletePostBtn: "button.settings-menu-delete-button",
@@ -26,11 +26,11 @@ const pageElements = {
     youtubeURLInput: "[placeholder='Paste URL to add embedded content...']",
   },
   page: {
-    pageView: "[href='#/pages/']",
+    pageView: ".gh-nav-list [href='#/pages/']",
     newPage: "[href='#/editor/page/']",
     pageTitle: "[placeholder='Page Title']",
     pageContent: "[data-placeholder='Begin writing your page...']",
-    newPagePreviousPage: "[href='#/pages/']",
+    newPagePreviousPage: ".gh-editor-header [href='#/pages/']",
     firstPageInList: ".gh-list > li:nth-child(2) .gh-content-entry-title",
     pageSettings: "button.post-settings",
     deletePageBtn: "button.settings-menu-delete-button",
@@ -44,14 +44,16 @@ const pageElements = {
       "#settings-navigation > .gh-blognav-item .gh-blognav-line span:last-of-type input",
     saveBtn: ".gh-btn-blue",
     lastNavBarItem: ".site-home-header ul.nav:first-of-type li:last-child a",
-    deleteButton: "#settings-navigation > .sortable-objects .draggable-object:last-child button",
+    deleteButton:
+      "#settings-navigation > .sortable-objects .draggable-object:last-child button",
     stayBtn: ".modal-footer button:first-of-type",
-    leaveBtn: ".modal-footer button:last-of-type"
+    leaveBtn: ".modal-footer button:last-of-type",
   },
   general: {
     linkSideMenu: "ul.gh-nav-settings li:nth-child(2)",
     expandTitleBtn: ".flex-column.mt2 > .gh-setting-first button",
-    titleSettingsInput: ".form-group:not(.description-container) .ember-text-field",
+    titleSettingsInput:
+      ".form-group:not(.description-container) .ember-text-field",
     descriptionSettingsInput: ".description-container .ember-text-field",
     saveBtn: ".gh-btn-blue",
   },
@@ -68,8 +70,8 @@ const pageElements = {
     userEditedName: ".apps-grid-cell:last-of-type .apps-card-app-title",
     saveNewPassBtn: ".button-change-password",
     userProfile: ".gh-user-avatar",
-    signout: ".user-menu-signout"
-  }
+    signout: ".user-menu-signout",
+  },
 };
 
 global.pageElements = pageElements;
